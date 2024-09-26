72a572ba458ba86ec7acaa6506345c1e8ac47932
nicholas <nicholasricardosilvaaraujo@gmail.com>
2024-09-26 16:42:22 -0400

    Aula Classe de Esatdo

--- src/App.js
+++ src/App.js
@@ -1,29 +1,89 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    // fragment React, não gera nada, apenas finje ser uma div Root
-    <>
+
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.handlePClick = this.handlePClick.bind(this);
+
+    // State é um objeto que armazena as informações que o componente manipula
+
+    this.state = {
+
+      name: 'Nicholas Ricardo'
+    };   
+  }
+
+  // Esta funnção é um método de instância que altera o estado do componente "name"
+  handlePClick() {
+    const { name } = this.state;
+
+    if (name === 'Nicholas Ricardo') {
+      this.setState({ name: 'Café' });
+    }else {
+      this.setState({ name: 'Nicholas Ricardo' });
+    }
+
+    // const { name } = this.state;
+    // console.log(`<p> ${name}`);
+  }
+
+  render() {
+
+    const { name } = this.state;
+
+
+
+    return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+          <p onClick={this.handlePClick}>
+            {name}
           </p>
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
-          >
+            >
             Learn React
           </a>
         </header>
       </div>
-    </>
-
-  );
+    );
+  }
 }
-
 export default App;
+
+
+
+
+// function App() {
+//   return (
+//     // fragment React, não gera nada, apenas finje ser uma div Root
+//     <>
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p>
+//             Edit <code>src/App.js</code> and save to reload.
+//           </p>
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//         </header>
+//       </div>
+//     </>
+
+//   );
+// }
